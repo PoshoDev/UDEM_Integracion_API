6f978e347a7a72d5c00d8d718f4ee3c6545918f1
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2021-11-29 02:09:47 -0600

    Api funciona LMAO

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import datetime as dt
 import loaf
 import json
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -71,18 +72,17 @@ def request_inventario():
     else:
         if marca == "None": q = loaf.call("AutomovilGetAll")
         else: q = loaf.call("AutomovilGetMarca", marca)
-    data = {}
-    data["inventario"] = []
+    inventario = []
     for elem in q:
-        data["inventario"].append({
+        inventario.append({
             "id":       elem[0],
-            "nombre":   elem[1],
+            "marca":    elem[1],
             "modelo":   elem[2],
             "color":    elem[3],
             "precio":   elem[4],
             "foto":     elem[5]
         })
-    return data
+    return jsonify(inventario)
 
 @app.route("/reserva/", methods=["GET"])
 def request_reserva():
@@ -97,41 +97,56 @@ def request_reserva():
     # ID(rip) Check™
     autos = []
     if id1 == "None": return error("Se requiere mínimo el id de un automóvil.")
-    autos.append(id1)
-    if id2 != "None": autos.append(id2)
-    if id3 != "None": autos.append(id3)
+    autos.append(int(id1))
+    if id2 != "None": autos.append(int(id2))
+    if id3 != "None": autos.append(int(id3))
+    print("AUTOS", autos)
     # Availability
     if desde=="None" or hasta=="None": return error("Se requiere un rango de fechas.")
     q = loaf.call("AutomovilGetFecha", desde, hasta)
+    print("AVAILABLES")
+    for elem in q:
+        print(elem)
     available = []
-    for elem in q: available.append(elem[0])
+    for elem in q: available.append(elem)
     count, suma = 0, 0
     for i in autos:
         for j in available:
             if i == j[0]:
                 count += 1
                 suma += j[4]
+    print("LAMOAO")
     if count != len(autos): return error("Uno o más ids de autos no están disponibles para estas fechas.")
     # Date ranges
-    check1 = datetime.strptime(f"{reciver[0]} 12:00:00",'%Y-%m-%d %H:%M:%S')
-    check2 = datetime.strptime(f"{reciver[1]} 12:00:00",'%Y-%m-%d %H:%M:%S')
-    delta = check2 - check1
+    check1 = dt.datetime.strptime(desde,'%Y-%m-%d')
+    print("CHECK1", check1)
+    check2 = dt.datetime.strptime(hasta,'%Y-%m-%d')
+    delta = (check2 - check1).days
+    print("DELTA", delta)
     if delta < 0: return error("Las fechas están en desorden.")
     suma *= delta
     # Bank request
     r = requests.put(f'http://bancoapi2.azurewebsites.net/api/Cuentas?cuenta1={cuenta}&pin={pin}&cuenta2=4255555557&dinero={suma}')
     try:
-        jeison = r.json()
-        print(jeison["Message"])
+        jobject = r.json()
+        banco_msg = jobject["Message"]
+        print("RESPONSE:", banco_msg)
     except ValueError:
+        print("ENTRE")
         usr = loaf.query(f"SELECT id FROM cliente WHERE usuario={cuenta}")
         if not usr:
             loaf.query(f"INSERT INTO cliente(usuario, nombre_completo) VALUES({cuenta}, 'Dr. Frixxxol')")
             usr = loaf.query(f"SELECT id FROM cliente WHERE usuario={cuenta}")
-        loaf.query(f"INSERT INTO reserva(cliente, fecha_desde, fecha_hasta) VALUES({usr}, '{desde}', '{hasta}')")
-        reserva = loaf.query(f"SELECT id FROM reserva ORDER BY id DESC LIMIT 1")
+        print(usr, desde, hasta)
+        loaf.query(f"INSERT INTO reserva(cliente, fecha_desde, fecha_hasta) VALUES({usr[0][0]}, '{desde}', '{hasta}')")
+        reserva = loaf.query(f"SELECT id FROM reserva ORDER BY id DESC LIMIT 1")[0][0]
         for elem in autos: loaf.query(f"INSERT INTO reserva_unidad VALUES({reserva}, {elem})")
-        return {"message":"¡Reserva exitosa!", "precio":suma, "reserva":reserva}
+        ret = {
+            "message": "¡Reserva exitosa!",
+            "precio": suma,
+            "reserva": reserva
+		}
+        return jsonify(ret)
     return error("Algo salió mal con el API de Bancos.")
 
 
